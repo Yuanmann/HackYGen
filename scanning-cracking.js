80d785fa07570d1d6b3cada72789b36a4215eec3
Yuanmann <nickbax5@gmail.com>
2015-06-11 01:32:02 -0500

    updated

---
+++ scanning-cracking.js
@@ -0,0 +1,34 @@
+﻿var scanLoops=100;
+var scanPercentage=0;
+var scanFill;
+var scanTimer=150; 
+
+function scanIps()
+{
+	if(scanPercentage==0)
+	{
+		myVar = setInterval(function(){progress()},scanTimer);
+	}
+	else
+	{
+		alert("already Scanning");
+	}
+
+	function progress()
+	{
+		if(scanLoops>0)
+		{
+			scanPercentage++;
+			fill=scanPercentage+"%";
+			$('.progress-bar').css("width",fill);
+			scanLoops--;
+		}
+		else
+		{
+		clearTimeout(myVar);
+		alert("done");
+		}
+	}
+
+}
+
