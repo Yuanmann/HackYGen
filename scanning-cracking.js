d55ed94b6167aaf26bcaca1907df46ff061ccaf5
Yuanmann <nickbax5@gmail.com>
2015-06-11 02:11:55 -0500

    updated

--- scanning-cracking.js
+++ scanning-cracking.js
@@ -1,7 +1,16 @@
-﻿var scanLoops=100;
+﻿var crackvar;
+var myVar;
+
+var scanLoops=100;
 var scanPercentage=0;
 var scanFill;
 var scanTimer=150; 
+
+var crackLoops=100;
+var crackPercentage=0;
+var crackFill;
+var crackTimer=150; 
+
 
 function scanIps()
 {
@@ -20,13 +29,48 @@ function scanIps()
 		{
 			scanPercentage++;
 			fill=scanPercentage+"%";
-			$('.progress-bar').css("width",fill);
+			$('#ipbar').css("width",fill);
 			scanLoops--;
 		}
 		else
 		{
 		clearTimeout(myVar);
-		alert("done");
+		scanPercentage=0;
+		scanLoops=100;
+		scanFill="0%";
+		$('#ipbar').css("width","0%");
+		}
+	}
+
+}
+
+function crackComputers()
+{
+	if(crackPercentage==0)
+	{
+		crackVar = setInterval(function(){progress()},crackTimer);
+	}
+	else
+	{
+		alert("already Cracking");
+	}
+
+	function progress()
+	{
+		if(crackLoops>0)
+		{
+			crackPercentage++;
+			fill=crackPercentage+"%";
+			$('#crackbar').css("width",fill);
+			crackLoops--;
+		}
+		else
+		{
+		clearTimeout(crackVar);
+		crackPercentage=0;
+		crackLoops=100;
+		crackFill="0%";
+		$('#crackbar').css("width","0%");
 		}
 	}
 
